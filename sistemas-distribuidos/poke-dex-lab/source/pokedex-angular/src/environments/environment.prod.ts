e59d57fd2ae86f9fdfe71f15009f4afb6d377de4
Mathew490 <140650838+Mathew490@users.noreply.github.com>
2025-04-23 18:30:00 -0500

    Update environment.prod.ts

--- sistemas-distribuidos/poke-dex-lab/source/pokedex-angular/src/environments/environment.prod.ts
+++ sistemas-distribuidos/poke-dex-lab/source/pokedex-angular/src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
   pokedexGithub: 'https://github.com/keilermora/pokedex-angular',
   imagesPath: '/assets/images',
   language: 'en',
-  languageId: 9,
+  languageId: 8,
 };
